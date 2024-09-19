e18cdb7fd20bbb8d5bdb17c1aac198f0e282aadc
LevIgnatev <ignatevlev1@gmail.com>
2024-09-19 10:39:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names;
+            names = new string[4];
+            names[0] = "a";
+            names[1] = "b";
+            names[2] = "c";
+            names[3] = "d";
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            string a = "ddd";
+            int b = 2384;
+            Console.WriteLine(
+                format: "{0,10} {1,6}",
+                arg0: a,
+                arg1: b);
+        }
+    }
+}
