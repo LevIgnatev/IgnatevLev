78e8ae2237fa87adc29f4b0365af39ad479dfc3f
LevIgnatev <ignatevlev1@gmail.com>
2024-10-13 12:30:05 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 0;
+            bool indicator = true;
+            Console.WriteLine("Введите число от 0 до 255");
+            try
+            {
+                double c = Convert.ToDouble(Console.ReadLine());
+                a = c;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введено НЕ число");
+                indicator = false;
+            }
+            if (a < 0 | a > 255)
+            {
+                Console.WriteLine("Число вне диапазона");
+                indicator = false;
+            }
+            Console.WriteLine("Введите число от 0 до 255");
+            try
+            {
+                double c = Convert.ToDouble(Console.ReadLine());
+                b = c;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Введено НЕ число");
+                indicator = false;
+            }
+            if (b < 0 | b > 255)
+            {
+                Console.WriteLine("Число вне диапазона");
+                indicator = false;
+            }
+            if (b == 0 & indicator == true)
+            {
+                Console.WriteLine("Деление на 0 невозможно");
+                indicator = false;
+            }
+            if (indicator == true)
+            {
+                Console.WriteLine(a / b);
+            }
+        }
+    }
+}
