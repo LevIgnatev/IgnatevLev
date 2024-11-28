512467204a8106afbbdb629ee025c2bce673d2d5
LevIgnatev <ignatevlev1@gmail.com>
2024-11-28 11:30:37 +0300

    Add files via upload

---
+++ Program8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    public class Fraction
+    {
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+        public Fraction(int numerator, int denominator)
+        {
+            Numerator = numerator;
+            Denominator = denominator;
+        }
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            Fraction resultfraction = new Fraction(fraction1.Numerator * fraction2.Numerator, fraction1.Denominator * fraction2.Denominator);
+            return resultfraction;
+        }
+    }
+    internal class Program8
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 3);
+            Console.WriteLine(f1 * f2);
+        }
+    }
+}
